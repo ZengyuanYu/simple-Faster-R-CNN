0fed4815a7774ddd2fa2b4fd7f857f5064a9a1a9
ZengyuanYu <1952284157@qq.com>
2019-01-15 16:57:54 +0800

    dataset

--- data/dataset.py
+++ data/dataset.py
@@ -1,3 +1,109 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
 # Author: Xiaoyu
+from __future__ import absolute_import
+from __future__ import division
+import torch as t
+from data.voc_dataset import VOCBboxDataset
+from skimage import transform as sktsf
+from torchvision import transforms as tvtsf
+from data import util
+import numpy as np
+from utils.config import opt
+
+
+def inverse_normalize(img):
+    if opt.caffe_pretrain:
+        img = img + (np.array([122.7717, 115.9465, 102.9801]).reshape(3, 1, 1))
+        return img[::-1, :, :]
+    return (img * 0.225 + 0.45).clip(min=0, max=1) * 255
+
+
+def pytorch_normalize(img):
+    normalize = tvtsf.Normalize(mean=[0.485, 0.456, 0.406],
+                                std=[0.229, 0.224, 0.225])
+    img = normalize(t.from_numpy(img))
+    return img.numpy()
+
+
+def caffe_normalize(img):
+    img = img[[2, 1, 0], :, :]  # RGB-BGR
+    img = img * 255
+    mean = np.array([122.7717, 115.9465, 102.9801]).reshape(3, 1, 1)
+    img = (img - mean).astype(np.float32, copy=True)
+    return img
+
+
+def preprocess(img, min_size=600, max_size=1000):
+    """
+    预处理图片等待特征提取
+    :param img: CHW RGB格式 值属于[0,255]
+    :return: 处理之后的图片
+    """
+    C, H, W = img.shape
+    scale1 = min_size / min(H, W)
+    scale2 = max_size / max(H, W)
+    scale = min(scale1, scale2)
+    img = img / 255.
+    img = sktsf.resize(img, (C, H * scale, W * scale), mode='reflect',
+                       anti_aliasing=False)
+    # 都应该小于min_size
+    if opt.caffe_pretrain:
+        normalize = caffe_normalize
+    else:
+        normalize = pytorch_normalize
+    return normalize(img)
+
+
+class Transform(object):
+
+    def __init__(self, min_size=600, max_size=1000):
+        self.min_size = min_size
+        self.max_size = max_size
+
+    def __call__(self, in_data):
+        img, bbox, label = in_data
+        _, H, W = img.shape
+        img = preprocess(img, self.min_size, self.max_size)
+        _, o_H, o_W = img.shape
+        scale = o_H / H
+        bbox = util.resize_bbox(bbox, (H, W), (o_H, o_W))
+
+        # 水平翻转
+        img, params = util.random_filp(
+            img, x_random=True, return_param=True)
+        bbox = util.filp_bbox(
+            bbox, (o_H, o_W), x_filp=params['x_filp'])
+        return img, bbox, label, scale
+
+
+class Dataset:
+    def __init__(self, opt):
+        self.opt = opt
+        self.db = VOCBboxDataset(opt.voc_data_dir)
+        self.tsf = Transform(opt.min_size, opt.max_size)
+
+    def __getitem__(self, idx):
+        ori_img, bbox, label, difficult = self.db.get_example(idx)
+        img, bbox, label, scale = self.tsf((ori_img, bbox, label))
+        return img.copy(), bbox.copy(), label.copy(), scale
+
+    def __len__(self):
+        return len(self.db)
+
+
+class TestDataset:
+    def __init__(self, opt, split='test', use_difficult=True):
+        self.opt = opt
+        self.db = VOCBboxDataset(opt.voc_data_dir, split=split, use_difficult=use_difficult)
+
+    def __getitem__(self, idx):
+        ori_img, bbox, label, difficult = self.db.get_example(idx)
+        img = preprocess(ori_img)
+        return img, ori_img.shape[1:], bbox, label, difficult
+
+    def __len__(self):
+        return len(self.db)
+
+
+
